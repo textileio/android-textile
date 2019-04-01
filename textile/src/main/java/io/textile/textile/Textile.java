2842fce173cd87a69825d96c82b1ef16af349044
Aaron Sutula <aaron.sutula@gmail.com>
2019-03-31 22:02:34 -0600

    WIP on top level Texitle api

--- textile/src/main/java/io/textile/textile/Textile.java
+++ textile/src/main/java/io/textile/textile/Textile.java
@@ -1,5 +1,6 @@
 package io.textile.textile;
 
+import io.textile.pb.View.Summary;
 import mobile.Mobile_;
 
 public class Textile {
@@ -9,15 +10,30 @@ public class Textile {
     Mobile_ node;
 
     private Textile () {
-
     }
-
 
     private static class TextileHelper {
         private static final Textile INSTANCE = new Textile();
     }
 
+    public static void initialize(boolean debug, boolean logToDisk) throws Exception {
+
+    }
+
     public static Textile instance() {
         return TextileHelper.INSTANCE;
+    }
+
+    public String version() {
+        return node.version();
+    }
+
+    public String gitSummary() {
+        return node.gitSummary();
+    }
+
+    public Summary summary() throws Exception {
+        byte[] bytes = node.summary();
+        return Summary.parseFrom(bytes);
     }
 }
